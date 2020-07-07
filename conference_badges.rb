79eede506a3df6189359b1c755c1d82502dad151
Michael Ciabuca <ciabuca@gmail.com>
2020-07-07 00:27:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,18 @@
 # Write your code here.
 
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
 end
 
+def batch_badge_creator(array)
+  
+  
+end
+
+def assign_rooms(array)
+  
+end
+
+def printer(array)
+  
+end
